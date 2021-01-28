2ab88f4dca997b5a99eb8a931eab1d5cc397cb33
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-27 20:49:25 -0800

    added foreign key and reference points for table columns

--- schema.sql
+++ schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -20,5 +21,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
