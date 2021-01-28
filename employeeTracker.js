93e50cf62248477b32def705b1e66bfd09b3f90c
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-27 20:12:13 -0800

    create initial options in runsearch function for user to choose what they would like to do

--- employeeTracker.js
+++ employeeTracker.js
@@ -17,3 +17,24 @@ connection.connect(function (err) {
     if (err) throw err;
     runSearch();
 });
+
+function runSearch() {
+    inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "Add role",
+            "Remove role",
+            "View department total salary"
+        ]
+    })
+}
