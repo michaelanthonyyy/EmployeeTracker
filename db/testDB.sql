7e328b37fa190be38d8d81afeece9ac285dec90c
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-27 21:49:22 -0800

    created table data for department and roles

---
+++ db/testDB.sql
@@ -0,0 +1,11 @@
+-- role - department 
+-- sales lead - sales
+-- salesperson - sales
+-- lead engineer - engineering
+-- software engineer - engineering
+-- account manageer - finance
+-- accountant - finance
+-- legal team lead - legal
+-- lawyer - legal
+
+
