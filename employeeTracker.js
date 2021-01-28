7ab9639d3e379e381f97831826f1e12d4bf2f987
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-28 00:12:08 -0800

    added role.title and department.name to var query in viewAllEmployeesByDepartment function for cleaner look when console.table runs

--- employeeTracker.js
+++ employeeTracker.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var cTable = require("console.table");
 
-var connection = mysql.createConnection ({
+var connection = mysql.createConnection({
     host: "localhost",
 
     port: 3306,
@@ -10,7 +10,7 @@ var connection = mysql.createConnection ({
     user: "root",
 
     password: "password",
-    
+
     database: "employee_listDB"
 });
 
@@ -21,98 +21,98 @@ connection.connect(function (err) {
 
 function runSearch() {
     inquirer
-    .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-            "View All Employees",
-            "View All Employees By Department",
-            "View All Employees By Manager",
-            "Add Employee",
-            "Remove Employee",
-            "Update Employee Role",
-            "Update Employee Manager",
-            "Add role",
-            "Remove role",
-            "View department total salary",
-            "Are you finished?"
-        ]
-    })
-    .then(function (answer) {
-        switch(answer.action) {
-            case "View All Employees":
-                viewAll();
-                break;
-            case "View All Employees By Department":
-                viewAllEmployeesByDepartment();
-                break;
-            case "View All Employees By Manager":
-                viewAllEmployeesByManager();
-                break;
-            case "Add Employee":
-                addEmployee();
-                break;
-            case "Remove Employee":
-                removeEmployee();
-                break;
-            case "Update Employee Role":
-                updateRole();
-                break;
-            case "Update Employee Manager":
-                updateManager();
-                break;
-            case "Add role":
-                addRole();
-                break;
-            case "Remove role":
-                removeRole();
-                break;
-            case "View department total salary":
-                departmentSalary();
-                break;
-            case "I'm Done":
-                connection.end();
-                break;
-        }
-    })
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "Add role",
+                "Remove role",
+                "View department total salary",
+                "Are you finished?"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    viewAll();
+                    break;
+                case "View All Employees By Department":
+                    viewAllEmployeesByDepartment();
+                    break;
+                case "View All Employees By Manager":
+                    viewAllEmployeesByManager();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateRole();
+                    break;
+                case "Update Employee Manager":
+                    updateManager();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Remove role":
+                    removeRole();
+                    break;
+                case "View department total salary":
+                    departmentSalary();
+                    break;
+                case "I'm Done":
+                    connection.end();
+                    break;
+            }
+        })
 };
 
 
 
 // View All Employees
-function viewAll(){
-    connection.query("SELECT * FROM employee", function(err, res) {
-        if(err) throw err;
+function viewAll() {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
         console.table(res);
         runSearch();
     })
 };
 
+
 // View Employees by Department
-function viewAllEmployeesByDepartment(){
-    inquirer 
-    .prompt({
-        type: "list",
-        name: "department",
-        message: "Which department would you like to see?",
-        choices: [
-            "Sales",
-            "Engineering",
-            "Finance",
-            "Legal"
-        ]
-    })
-    .then((answer) => {
-        var query = "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id FROM ((employee INNER JOIN role On employee.role_id = role.id) INNER JOIN department ON department.id = role.department_id) WHERE department.name = ?";
-        connection.query(query, [answer.department],
-            function(err, res) {
-                if(err) throw err;
-                console.table(res);
-                runSearch();
-            })
-    }) 
-   
+function viewAllEmployeesByDepartment() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "department",
+            message: "Which department would you like to see?",
+            choices: [
+                "Sales",
+                "Engineering",
+                "Finance",
+                "Legal"
+            ]
+        })
+        .then((answer) => {
+            var query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name,  employee.manager_id FROM ((employee INNER JOIN role On employee.role_id = role.id) INNER JOIN department ON department.id = role.department_id) WHERE department.name = ?";
+            connection.query(query, [answer.department],
+                function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    runSearch();
+                })
+        })
 };
 
 
